f79033f8514e4b35250f8eb29526d62f85330d28
Rafiul <rafiulislam1020@gmail.com>
2025-06-30 13:20:47 +0600

    logout confirmation dialog added

--- src/app/pages/private/component/header/header.ts
+++ src/app/pages/private/component/header/header.ts
@@ -2,21 +2,43 @@ import { Component, effect } from '@angular/core';
 import { ApiService } from '../../../../services/api.service';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, ButtonModule],
+  imports: [RouterModule, ButtonModule, ConfirmDialogModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrls: ['./header.scss'],
+  providers: [ConfirmationService]
 })
 export class Header {
 
   userData: any = null;
 
-  constructor(private apiService: ApiService) {
+  constructor(
+    private apiService: ApiService,
+    private confirmationService: ConfirmationService
+  ) {
     effect(() => {
       this.userData = this.apiService.loginState().user;
     });
+  }
+
+  confirmLogout = () => {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to log out?',
+      header: 'Confirm Logout',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Yes',
+      rejectLabel: 'No',
+      acceptButtonStyleClass: 'p-button-danger p-button-sm',
+      rejectButtonStyleClass: 'p-button-secondary p-button-sm',
+      accept: () => {
+        this.logout();
+      }
+    });
+
   }
 
   logout = () => {
