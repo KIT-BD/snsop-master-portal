062d054b996c2f0eed259b3fbc56aa77501620a7
Rafiul Islam <rafiulislam1020@gmail.com>
2025-07-07 14:14:53 +0600

    deployment fix

--- src/app/auth-guard.ts
+++ src/app/auth-guard.ts
@@ -2,17 +2,18 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
-export const authGuard: CanActivateFn = (route:any, state:any) => {
+export const authGuard: CanActivateFn = (route: any, state: any) => {
   const cookieService = inject(CookieService);
   const router = inject(Router);
-  let lState: boolean = true;
   const ACCESS_TOKEN = '_kit_ta';
-  const isLoggedIn = cookieService.get(ACCESS_TOKEN) ? true : false;
+  const isLoggedIn = !!cookieService.get(ACCESS_TOKEN);
+
   console.log('authGuard', isLoggedIn);
   console.log('authGuard', route.routeConfig.path);
+
   if (isLoggedIn) {
-    lState = false;
-    router.navigateByUrl('/private');
+    return router.createUrlTree(['/private']);
+  } else {
+    return true;
   }
-  return lState;
 };
